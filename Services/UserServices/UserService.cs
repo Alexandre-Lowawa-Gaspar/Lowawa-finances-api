b555d16493216bd8e282d1f0bae001dea809fe4f
Alexandre-Lowawa-Gaspar <alexandreshmurda@gmail.com>
2023-11-24 14:28:11 +0100

    Update UserService.cs

--- Services/UserServices/UserService.cs
+++ Services/UserServices/UserService.cs
@@ -9,30 +9,52 @@ namespace Lowawa_finances_api.Services.UserServices
     {
         private static List<User> users = new List<User>();
         private readonly IMapper _mapper;
-
-        public UserService(IMapper mapper)
+        private readonly DataContext _context;
+        public UserService(IMapper mapper, DataContext context)
         {
             _mapper = mapper;
+            _context = context;
+
         }
         public async Task<ServicesResponse<List<GetUserDto>>> AddUser(AddUserDto newUser)
         {
             var servicesResponse = new ServicesResponse<List<GetUserDto>>();
-            var user = _mapper.Map<User>(newUser);
-            if(users.Count==0)
-            user.Id = 1;
-            else
-            user.Id=users.Max(x => x.Id)+1;
-            users.Add(user);
-            servicesResponse.Data = users.Select(c => _mapper.Map<GetUserDto>(c)).ToList();
+            try
+            {
+                var user = _mapper.Map<User>(newUser);
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+                servicesResponse.Data = await _context.Users.Select(c => _mapper.Map<GetUserDto>(c)).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                servicesResponse.Success = false;
+                servicesResponse.Message = ex.Message.Replace("'", "");
+            }
             return servicesResponse;
         }
 
         public async Task<ServicesResponse<List<GetUserDto>>> DeleteUser(int id)
         {
             var servicesResponse = new ServicesResponse<List<GetUserDto>>();
-            var user = users.First(c => c.Id == id);
-            users.Remove(user);
-            servicesResponse.Data = users.Select(c => _mapper.Map<GetUserDto>(c)).ToList();
+            try
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(c => c.Id == id);
+                if (user is null)
+                {
+                    servicesResponse.Success = false;
+                    servicesResponse.Message = new Exception($"The user with Id {id} was not found.").Message.Replace("'", "");
+                    return servicesResponse;
+                }
+                users.Remove(user);
+                await _context.SaveChangesAsync();
+                servicesResponse.Data = users.Select(c => _mapper.Map<GetUserDto>(c)).ToList();
+            }
+            catch (Exception ex)
+            {
+                servicesResponse.Success = false;
+                servicesResponse.Message = ex.Message.Replace("'", "");
+            }
             return servicesResponse;
         }
 
@@ -42,8 +64,10 @@ namespace Lowawa_finances_api.Services.UserServices
 
             try
             {
-            servicesResponse.Data = users.Select(c => _mapper.Map<GetUserDto>(c)).ToList();
-            }catch(Exception ex){
+                servicesResponse.Data = await _context.Users.Select(c => _mapper.Map<GetUserDto>(c)).ToListAsync();
+            }
+            catch (Exception ex)
+            {
                 servicesResponse.Success = false;
                 servicesResponse.Message = ex.Message.Replace("'", "");
             }
@@ -53,19 +77,36 @@ namespace Lowawa_finances_api.Services.UserServices
         public async Task<ServicesResponse<GetUserDto>> GetUserById(int id)
         {
             var servicesResponse = new ServicesResponse<GetUserDto>();
-            var user = users.FirstOrDefault(c => c.Id == id);
-            servicesResponse.Data = _mapper.Map<GetUserDto>(user);
+            try
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(c => c.Id == id);
+                servicesResponse.Data = _mapper.Map<GetUserDto>(user);
+            }
+            catch (Exception ex)
+            {
+                servicesResponse.Success = false;
+                servicesResponse.Message = ex.Message.Replace("'", "");
+            }
             return servicesResponse;
         }
 
         public async Task<ServicesResponse<List<GetUserDto>>> UpdateUser(UpdateUserDto updateUser)
         {
             var servicesResponse = new ServicesResponse<List<GetUserDto>>();
-            var user = users.FirstOrDefault(x => x.Id == updateUser.Id);
-            if (user is null)
-                throw new Exception($"User with Id {updateUser.Id} not found.");
-            _mapper.Map(updateUser, user);
-            servicesResponse.Data = users.Select(c => _mapper.Map<GetUserDto>(user)).ToList();
+            try
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == updateUser.Id);
+                if (user is null)
+                    throw new Exception($"User with Id {updateUser.Id} not found.");
+                _mapper.Map(updateUser, user);
+                await _context.SaveChangesAsync();
+                servicesResponse.Data = await _context.Users.Select(c => _mapper.Map<GetUserDto>(user)).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                servicesResponse.Success = false;
+                servicesResponse.Message = ex.Message.Replace("'", "");
+            }
             return servicesResponse;
         }
     }
